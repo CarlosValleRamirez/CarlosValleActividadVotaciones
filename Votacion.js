820a33395c0e532877e1899b50782bd444133480
CarlosValleRamirez <Carlos_Valle_Ramirez@hotmail.com>
2020-11-16 17:15:11 -0600

    Votacion

--- Votacion.js
+++ Votacion.js
@@ -2,6 +2,10 @@ let contador = 3;
 
 function masUno() {
   contador = contador + 1;
+  document.getElementById("votos").innerHTML = contador + "votos";
+}
 
-  document.getElementById("votos").innerHTML = contador + " votos";
+function menosUno() {
+  contador = contador - 1;
+  document.getElementById("votos").innerHTML = contador + "votos";
 }
